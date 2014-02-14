2c134eece5635684e952412147c9d0bd302dd4a9
Peter Haggerty <peter@librato.com>
2014-02-13 20:25:16 -0500

    Forgot to merge metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Creates users from a databag search"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.3.1"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 recipe            "users", "reads users data bag and creates users"
 recipe            "users::sysadmins", "searches users data bag for sysadmins and creates users"
-=======
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Creates users from a databag search"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-recipe "users::sysadmin", "searches users data bag for sysadmins and creates users"
+version           "1.0.0"
 
 %w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
 end
->>>>>>> master
